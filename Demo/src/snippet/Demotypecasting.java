b555aa0ed5e042b683e079beb31a47ffb9148a0c
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 15:16:24 +0530

    10

---
+++ Demo/src/snippet/Demotypecasting.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Demotypecasting {
+public static void main(String[] args) {
+	int i= 10; // variable with f is a type of integer which is holding value of 10
+	 long l = (long) i; // defining a var with name l which of datatype long = convert 
+	
+	 System.out.println(l);
+	}
+}
