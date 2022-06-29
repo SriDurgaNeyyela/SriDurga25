f93d0978fe19cdc2ec1f3b5a73e93b7bdc8e8acc
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 15:33:13 +0530

    0

---
+++ Demo/src/snippet/Demoternary.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Demoternary {
+
+	public static void main(String[] args) {
+		{
+			int x = 11;
+			x = (10 == x) ? 1 : 0; // condition ?: True:False
+			System.out.println(x);
+			 
+			}
+	}
+}
