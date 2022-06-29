e214032e052b1606a96204a082eb77375496d801
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 15:18:41 +0530

    30 9 21

---
+++ Demo/src/snippet/Demoarthemetic.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Demoarthemetic {
+
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		System.out.println(x+y); //30
+		System.out.println(--x); // 9
+		System.out.println(++y); //21
+		}
+		}
+	
