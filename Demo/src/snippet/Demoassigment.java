8febda46d17bec77fd2e6ef2e9892b7684942c85
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 15:35:43 +0530

    4

---
+++ Demo/src/snippet/Demoassigment.java
@@ -0,0 +1,15 @@
+package snippet;
+
+public class Demoassigment {
+
+	public static void main(String[] args) {
+		int x = 4;
+		//x += 5; // x = x + 5
+		//x -= 5; // x = x - 5
+		//x *= 5; // x = x * 5
+		//x /= 4; // x = x / 4
+		x %= 5; // x = x % 5
+		System.out.println(x);
+		}
+		}
+		
